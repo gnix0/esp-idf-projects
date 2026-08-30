41df0a9f486abc28a10328fd296109ec441b06a1
gnix0 <dev.gustavoa@gmail.com>
2026-08-30 16:20:05 -0300

    include task deletion examples

--- task/task_intro.c
+++ task/task_intro.c
@@ -7,40 +7,61 @@
 #include <stdbool.h>
 #include <stdint.h>
 
-static const char TAG[] = "Test task: ";
+static const char TAG1[] = "TASK APP MAIN: ";
+static const char TAG3[] = "TASK BLINK: ";
+static const char TAG2[] = "TASK COUNTER: ";
 
 #define LED_PIN 23
 
-TaskHandle_t xTaskBlinkHandle = NULL;
+TaskHandle_t xTaskBlinkHandle   = NULL;
+TaskHandle_t xTaskCounterHandle = NULL;
 
 void vTaskBlink(void *pvParameters);
+void vTaskCounter(void *pvParameters);
 
 void app_main(void)
 {
-    ESP_LOGI(TAG, "initializing application");
+    ESP_LOGI(TAG1, "initializing application");
 
+    xTaskCreate(vTaskCounter, "TASK_COUNTER", 2048, NULL,
+                2, &xTaskCounterHandle);
     xTaskCreate(vTaskBlink, "TASK_BLINK", 2048, NULL,
                 1, &xTaskBlinkHandle);
-
-    uint16_t counter = 0;
-    for (;;) {
-        ESP_LOGI(TAG, "TASK MAIN - Counter: %d", counter);
-        vTaskDelay(pdMS_TO_TICKS(3000));
-        counter++;
-    }
 }
 
 void vTaskBlink(void *pvParameters)
 {
     bool status = false;
 
-    ESP_LOGI(TAG, "initializing task Blink");
+    ESP_LOGI(TAG2, "initializing task Blink");
     gpio_reset_pin(LED_PIN);
     gpio_set_direction(LED_PIN, GPIO_MODE_OUTPUT);
 
     for (;;) {
         gpio_set_level(LED_PIN, status^=1);
-        ESP_LOGI(TAG, "TASK BLINK LED[%d]: %d", LED_PIN, status);
-        vTaskDelay(pdMS_TO_TICKS(3000));
+        ESP_LOGI(TAG2, "LED[%d] = %d", LED_PIN, status);
+        vTaskDelay(pdMS_TO_TICKS(1500));
+    }
+}
+
+void vTaskCounter(void *pvParameters)
+{
+    uint16_t counter = 0;
+    ESP_LOGI(TAG3, "initializing task Counter");
+
+    for (;;) {
+        ESP_LOGI(TAG3, "Counter = %d", counter);
+
+        if (counter == 10) {
+            vTaskDelete(xTaskBlinkHandle);
+            gpio_set_level(LED_PIN, 0);
+        } else if (counter == 15) {
+            xTaskCreate(vTaskBlink, "TASK_BLINK", 2048, NULL,
+                        1, &xTaskBlinkHandle);
+            vTaskDelete(NULL);  // deletes itself, same as passing vTaskCounterHandle instead
+        }
+
+        vTaskDelay(pdMS_TO_TICKS(1500));
+        counter++;
     }
 }
