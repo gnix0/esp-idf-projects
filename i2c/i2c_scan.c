912a56ad78cb552b4481e49f44c3a296969d888b
gnix0 <dev.gustavoa@gmail.com>
2026-09-08 09:33:16 -0300

    feat: introduce error handling

--- i2c/i2c_scan.c
+++ i2c/i2c_scan.c
@@ -19,6 +19,8 @@ static const char *TAG = "I2C Scanner Demo";
 
 void app_main(void)
 {
+    esp_err_t ret;
+
     ESP_LOGI(TAG, "Starting I2C scanning demo...");
 
     i2c_config_t i2c_cfg = {
@@ -29,23 +31,62 @@ void app_main(void)
         .scl_pullup_en      = GPIO_PULLUP_ENABLE,
         .master.clk_speed   = I2C_MASTER_FREQ_HZ,
     };
-    i2c_param_config(I2C_MASTER_NUM, &i2c_cfg);
-    i2c_driver_install(I2C_MASTER_NUM, i2c_cfg.mode, I2C_MASTER_RX_BUF_DISABLE,
-                        I2C_MASTER_TX_BUF_DISABLE, 0);
+    ret = i2c_param_config(I2C_MASTER_NUM, &i2c_cfg);
+    if (ret == ESP_ERR_INVALID_ARG) {
+        ESP_LOGE(TAG, "i2c parameters configuration failed (%s)", esp_err_to_name(ret));
+        abort();
+    }
+
+    ret = i2c_driver_install(
+        I2C_MASTER_NUM,
+        i2c_cfg.mode,
+        I2C_MASTER_RX_BUF_DISABLE,
+        I2C_MASTER_TX_BUF_DISABLE,
+        0
+    );
+    if (ret == ESP_ERR_INVALID_ARG) {
+        ESP_LOGE(TAG, "i2c driver install failed (%s)", esp_err_to_name(ret));
+        abort();
+    }
     
     ESP_LOGI(TAG, "I2C started");
 
     ESP_LOGI(TAG, "Scanning I2C addresses...");
     for (uint8_t i = 0; i < 128; i++) {
-        i2c_cmd_handle_t cmd = i2c_cmd_link_create();                                   // Create a command link
-        i2c_master_start(cmd);                                                          // Start the command link
-        i2c_master_write_byte(cmd, (i << 1) | I2C_MASTER_WRITE, ACK_CHECK_EN);          // Write the address to the slave
-        i2c_master_stop(cmd);                                                           // Stop the command link
+        // Create a command link
+        i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+        if (cmd == NULL) {
+            ESP_LOGE(TAG, "Command link couldn't be created: insufficient dynamic memory");
+            abort();
+        }
 
-        esp_err_t ret = i2c_master_cmd_begin(I2C_MASTER_NUM, cmd, pdMS_TO_TICKS(10));   // Send the command link
+        // Start the command link
+        ret = i2c_master_start(cmd);
+        if (ret == ESP_ERR_INVALID_ARG) {
+            ESP_LOGE(TAG, "i2c driver install failed (%s)", esp_err_to_name(ret));
+            abort();
+        }
+        
+        // Write the address to the slave
+        ret = i2c_master_write_byte(cmd, (i << 1) | I2C_MASTER_WRITE, ACK_CHECK_EN);    
+        if (ret == ESP_ERR_INVALID_ARG) {
+            ESP_LOGE(TAG, "writing the address to the slave failed (%s)", esp_err_to_name(ret));
+            abort();
+        }
+
+        // Stop the command link
+        ret = i2c_master_stop(cmd);
+        if (ret == ESP_ERR_INVALID_ARG) {
+            ESP_LOGE(TAG, "stopping the command link failed (%s)", esp_err_to_name(ret));
+            abort();
+        }
+
+        // Send the command link
+        ret = i2c_master_cmd_begin(I2C_MASTER_NUM, cmd, pdMS_TO_TICKS(10));
         if (ret == ESP_OK)
             ESP_LOGI(TAG, "Found=0x%x", i);
         
+        // Delete the command link
         i2c_cmd_link_delete(cmd);
     }
 
